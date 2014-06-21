d32ad61e39e3248271a4d3d2b05b0cc888235138
Harald Lapp <h.lapp@clipdealer.de>
2014-06-21 19:18:02 +0200

    use appropriate method to return trace

--- libs/logger/writer/graylog.class.php
+++ libs/logger/writer/graylog.class.php
@@ -113,7 +113,7 @@ namespace org\octris\core\logger\writer {
                 'short_message' => $message['message'],
                 'full_message'  => (is_null($message['exception'])
                                     ? ''
-                                    : print_r($message['exception']->getTrace(), true)),
+                                    : $message['exception']->getTraceAsString()),
                 'timestamp'     => $message['timestamp'],
                 'level'         => self::$graylog_levels[$message['level']],
                 'facility'      => $message['facility'],
