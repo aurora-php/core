45114d1d78a42ea381d8d5d4932e60e6b2d642f2
Harald Lapp <harald@octris.org>
2014-10-12 00:30:38 +0200

    bugfix: use __DIR__ to reference current directory

--- libs/app/cli.class.php
+++ libs/app/cli.class.php
@@ -13,8 +13,8 @@ namespace org\octris\core\app {
     use \org\octris\core\validate as validate;
     use \org\octris\core\provider as provider;
     
-    require_once('../app.class.php');
-    require_once('cli/autoloader.class.php');
+    require_once(__DIR__ . '/../app.class.php');
+    require_once(__DIR__ . '/cli/autoloader.class.php');
 
     /**
      * Core class for CLI applications.
