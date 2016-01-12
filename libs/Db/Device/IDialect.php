87690c063fdfae5dc7be070ea1a1d183c12d107a
Harald Lapp <h.lapp@clipdealer.de>
2016-01-12 13:26:54 +0100

    initial commit interface for implementing SQL dialect specific methods

---
+++ libs/Db/Device/IDialect.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the 'octris/core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Core\Db\Device;
+
+/**
+ * Interface for implementing SQL dialects.
+ *
+ * @copyright   copyright (c) 2016 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+interface IDialect
+{
+    /**
+     * Return LIMIT string.
+     */
+    public function getLimitString($offset, $limit);    
+}
